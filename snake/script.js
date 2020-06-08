3046d744eb298ca9019bf321d5d973e254090622
Rogério <rjr.rogerio@gmail.com>
2020-06-08 08:30:44 -0300

    snake movement

--- snake/script.js
+++ snake/script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "rigth";
 
 
 function createBG(){
@@ -15,11 +16,34 @@ function createBG(){
 
 function createSnake(){
     for (i=0; i < snake.length; i++){
-        context.fillStyle = "green"
-        context.fillRect(snake[i].x, snake[i].y, box, box)
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-createBG();
-createSnake();
+function initGame(){
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "rigth") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "down") snakeX += box;
+    if(direction == "up") snakeX -= box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    
+    snake.unshift(newHead);
+
+
+}
+
+let game = setInterval(initGame, 100);
 
