2cfe0483e365757ef3f460fd4603fd250b7b26c1
Rogério <rjr.rogerio@gmail.com>
2020-06-08 09:35:55 -0300

    Finishing game

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ let food = {
     y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-
 function createBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);   
@@ -36,7 +35,6 @@ function update(event){
     if(event.keyCode == 38 && direction != 'down') direction = 'up';
     if(event.keyCode == 39 && direction != 'left') direction = 'right';
     if(event.keyCode == 40 && direction != 'up') direction = 'down';
-    console.log(event.keyCode);
 }
 
 
@@ -47,6 +45,12 @@ function initGame(){
     if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
      
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert('Fim de jogo');
+        }
+    }
 
     createBG();
     createSnake();
@@ -60,7 +64,13 @@ function initGame(){
     if (direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
